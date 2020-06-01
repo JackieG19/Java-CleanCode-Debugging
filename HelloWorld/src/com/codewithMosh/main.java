ec14860b730cad25b6e02ba0f92118fc11b29428
Jackie <JGtheophila19@gmail.com>
2020-05-31 21:32:13 -0400

    need to refactor the methods

--- HelloWorld/src/com/codewithMosh/main.java
+++ HelloWorld/src/com/codewithMosh/main.java
@@ -1,18 +1,50 @@
 package com.codewithMosh;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class main {
 	public static void main (String[] args){
-		greetUser("Jackie");
-		greetUser("Jenny");
+		final byte MONTHS_IN_YEAR = 12;
+		final byte PERCENT = 100;
 		
-		greetUser("John", "Smith");
-	}
-	
-	public static void greetUser(String name) {
-		System.out.println("Hello " + name);
-	}
-
-	public static void greetUser(String firstName, String lastName) {
-		System.out.println("Hello " + firstName + " " + lastName);
+		int principal = 0;
+		float monthyInterest = 0;
+		int numberOfPayment = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true){
+			System.out.print("Principle: ");
+			principal = scanner.nextInt();
+			if(principal >= 1000 && principal <= 1_000_000);
+				break;
+			System.out.println("Enter a value between 1000 and 1,000,000");
+		}
+		
+		while(true){
+			System.out.print("Annual Interest Rate: ");
+			float annualInterest = scanner.nextFloat();
+			if(annualInterest >= 1 && annualInterest <= 30);
+			monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+				break;
+			System.out.println("Enter a value between 1 and 30");
+		}
+		
+		while(true){
+			System.out.print("Period (Years): ");
+			byte years = scanner.nextByte();
+			if(years >= 1 && years <= 30);
+			numberOfPayment = years * MONTHS_IN_YEAR;
+				break;
+			System.out.println("Enter a value between 1 and 30");
+		}
+		
+		double mortgage = principal
+				* (monthlyInterest * Math.pow(1 + monthlyInterest))
+				/ (Math.pow(1 + monthlyInterest, numberOfPayment))
+				
+		String mortgageFormatted = NumberFormat.getCurrencyInstance();
+		System.out.println("Mortgage: "+ mortgageFormatted);
 	}
 }
